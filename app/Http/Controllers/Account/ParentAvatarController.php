60df2e0fbcfef3d285f223ea353ee2a225695420
Matthew Gilpin <matty0005@gmail.com>
2021-08-14 12:04:04 +1000

    Added compression to avatar image + change login background

--- app/Http/Controllers/Account/ParentAvatarController.php
+++ app/Http/Controllers/Account/ParentAvatarController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request as InertiaRequest;
+use Intervention\Image\ImageManagerStatic as Image;
 
 
 class ParentAvatarController extends Controller
@@ -20,7 +21,13 @@ class ParentAvatarController extends Controller
         ]);
 
         $imagedata = file_get_contents($data['avatar']);
-        $base64 = 'data:image/png;base64,' . base64_encode($imagedata);
+
+        $image_resize = Image::make($imagedata);              
+        $image_resize->resize(100, 100);
+        // $base64 = (string) $image_resize->encode('data-url'); 
+        $base64 = (string) $image_resize->encode('jpg', 50)->encode('data-url'); 
+
+        // $base64 = 'data:image/png;base64,' . base64_encode($imagedata);
         
         DB::table('user_settings')
             ->where('user_id', Auth::user()->id)
