4fdafde9f1d7265ece8fdd69357dd319604edfb3
BozenX <bozen0@hotmail.com>
2021-08-15 17:48:09 +1000

    added avatars to threads and messages

--- resources/js/Pages/Forum/Thread.vue
+++ resources/js/Pages/Forum/Thread.vue
@@ -1,5 +1,7 @@
 <template>
-    <div @click="visitThread" class="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-50 cursor-pointer">
+    <div @click="visitThread" class="bg-white overflow-hidden shadow rounded-lg" 
+        :class="clickable ? ' hover:bg-gray-50 cursor-pointer ':''"
+        >
         <div class="px-4 py-5 sm:p-6">
             <div class="m-3">
 
@@ -10,15 +12,20 @@
                     <p class="ml-auto" v-if="thread.topic_on_dashboard"> posted in {{thread.topic_on_dashboard}} </p>
                 </div>
                 <div class="border-2 border-parent-300 rounded-md p-4 mb-6">
-                    <div class="font-bold text-parent-600">
-                        {{thread.display_name}} <span class="text-sm mb-4 ">posted at {{time}}</span>
+                    <div class="font-bold text-parent-600 flex flex-row">
+
+                        <img class="rounded-lg mr-3" :src="thread.avatar"/>
+
+                        <div> {{thread.display_name}} <span class="text-sm mb-4 ">posted at {{time}}</span></div>
                     </div>
                      <div class="mt-2 mb-2 ml-4">
                         {{thread.comment}}
                     </div>
                 </div>
                 
-                {{thread.count}} total comments
+                <p v-if="thread.count >= 0"> {{thread.count}} total comments </p>
+
+                <slot/>
 
             </div>
         </div>
@@ -31,10 +38,20 @@
 import axios from 'axios'
 
 export default {
-    props: ['thread'],
+    props: {
+        'thread': Object,
+        'clickable': {
+            type: Boolean,
+            default: true,
+        },
+    },
 
     methods: {
         visitThread() {
+            if (!this.clickable) {
+                return;
+            }
+
             this.$inertia.visit(`/forum/topic/${this.thread.thread_topic_title}/${this.thread.id}`)
         },
     },
--- resources/js/Pages/Forum/ThreadPage.vue
+++ resources/js/Pages/Forum/ThreadPage.vue
@@ -1,54 +1,42 @@
 <template>
     <Dashboard :topics="topics">
-        <div v-if="thread" class="bg-white overflow-hidden shadow rounded-lg">
-            <div class="px-4 py-5 sm:p-6">
-                <div class="m-3">
-
-
-                    <div class="font-bold text-gray-700 text-xl mb-2"> {{thread.title}} </div>
-                    <div class="border-2 border-parent-300 rounded-md p-4 mb-6">
-                        <div class="font-bold text-parent-600">
-                            {{thread.display_name}} <span class="text-sm mb-4 ">posted at {{threadTime}}</span>
-                        </div>
-                        <div class="mt-2 mb-2 ml-4">
-                            {{thread.comment}}
-                        </div>
+        <Thread v-if="thread" :thread="thread" :clickable="false">
+            <div class="flex flex-row my-4 ">
+                <div class="flex-grow">
+                    <label for="message" class="block text-sm font-medium text-gray-700">Reply</label>
+                    <div class="mt-1">
+                        <input type="text" v-model="msg" name="message" id="message" class="shadow-sm focus:ring-parent-500 focus:border-parent-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="message">
                     </div>
-
-                    <div class="flex flex-row my-4 ">
-                        <div class="flex-grow">
-                            <label for="message" class="block text-sm font-medium text-gray-700">Reply</label>
-                            <div class="mt-1">
-                                <input type="text" v-model="msg" name="message" id="message" class="shadow-sm focus:ring-parent-500 focus:border-parent-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="message">
-                            </div>
-                        </div>
-
-                        <button class="bg-parent-300 hover:bg-parent-400 rounded-md px-8 py-2 mt-6  text-sm mx-8" @click="sendMsg">Reply </button>
-                    </div>
-                    
-                    <div v-for="(message, index) in messages" :key="index"> 
-                        <div class="font-bold">
-                            {{message.display_name}} <span class="text-sm mb-4 text-gray-600">posted at {{time(message)}}</span>
-                        </div>
-                        <div class="mt-2 mb-6 ml-4">
-                            {{message.message}}
-                        </div>
-                    </div> 
-
                 </div>
+
+                <button class="bg-parent-300 hover:bg-parent-400 rounded-md px-8 py-2 mt-6  text-sm mx-8" @click="sendMsg">Reply </button>
             </div>
-        </div>
+            
+            <div v-for="(message, index) in messages" :key="index"> 
+                <div class="font-bold flex flex-row">
+                    
+                    <img class="rounded-lg mr-3" :src="message.avatar" />
+
+                    <div>{{message.display_name}} <span class="text-sm mb-4 text-gray-600">posted at {{time(message)}}</span></div>
+                </div>
+                <div class="mt-2 mb-6 ml-10">
+                    {{message.message}}
+                </div>
+            </div> 
+        </Thread>
     </Dashboard>
 </template>
 
 <script>
 
 import Dashboard from './Dashboard.vue'
+import Thread from './Thread.vue'
 
 export default {
 
     components: {
         Dashboard,
+        Thread,
     },
 
     props: {
@@ -89,11 +77,5 @@ export default {
             return "";
         },
     },
-
-    computed:  {
-        threadTime() {
-            return this.time(this.thread);
-        },
-    }
 }
 </script>
