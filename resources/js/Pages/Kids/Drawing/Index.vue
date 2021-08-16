7b8f8f8bfce08d2d8323aa6fdf3465a51d28bb4a
Matthew Gilpin <matty0005@gmail.com>
2021-08-16 23:22:09 +1000

    Added logo to register /forgot password pages + bug fixes

--- resources/js/Pages/Kids/Drawing/Index.vue
+++ resources/js/Pages/Kids/Drawing/Index.vue
@@ -28,9 +28,9 @@
                 <div @click="changeStrokeSize(20)" class="flex justify-center items-center">
                     <svg xmlns="http://www.w3.org/2000/svg"  class="h-8 w-8 mx-1"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><circle cx="12" cy="12" r="10"></circle></svg>
                 </div>
-                <div class="flex-grow"  >
+                <div class="flex-grow">
                 </div>
-                <div class=""  @click="clear">
+                <div class="" @click="clear">
                     <svg class="ml-auto flex w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
                 </div>
 
@@ -77,12 +77,10 @@
 
                 this.canvas.style.width ='100%';
                 this.canvas.style.height='80vh';
-                // ...then set the internal size to match
+                // set the internal size to match
                 this.canvas.width  = this.canvas.offsetWidth;
                 this.canvas.height = this.canvas.offsetHeight;
 
-                // this.canvas.height = window.innerHeight / 2
-                // this.canvas.width = window.innerWidth / 2
                 this.ctx.lineWidth = 5;
 
                 window.addEventListener('resize', this.updateCanvasSize);
@@ -149,7 +147,7 @@
                 console.log("Yeet")
                 this.canvas.style.width ='100%';
                 this.canvas.style.height='80vh';
-                // ...then set the internal size to match
+                // set the internal size to match
                 this.canvas.width  = this.canvas.offsetWidth;
                 this.canvas.height = this.canvas.offsetHeight;
 
