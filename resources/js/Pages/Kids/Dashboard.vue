fcf8783e27a8d00697bc4619ed1fb00f1e0b4799
samantha-tran <stran322001@gmail.com>
2021-09-15 21:19:04 +1000

    restyled lock svg and fixed mobile responsiveness

--- resources/js/Pages/Kids/Dashboard.vue
+++ resources/js/Pages/Kids/Dashboard.vue
@@ -1,11 +1,11 @@
 <template>
   <layout class="bg-gray-100 min-h-screen">
     <div class="container m-auto">
-        <div class="mt-10 mb-20">
+        <div class="mb-20">
             <h1 class="text-center text-9xl">Seedlings</h1>
             <h2 class="text-center text-5xl">for kids</h2>
         </div>
-        <div class="flex flex-col md:flex-row">
+        <div class="flex flex-col items-center md:flex-row">
             <div class="mx-auto">
                 <Link href="/kids/activities">
                     <div class="bg-white bg-opacity-90 rounded-full h-64 w-64 mx-auto">
@@ -15,7 +15,7 @@
                     />
                     </div>
                 </Link>
-                <h3 class="py-2 my-6 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl">Activities</h3>
+                <h3 class="py-2 my-6 w-64 mb-24 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl md:mb-6">Activities</h3>
             </div>
             <div>
                 <Link href="/kids/draw">
@@ -26,7 +26,7 @@
                     />
                     </div>
                 </Link>
-                <h3 class="py-2 my-6 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl">Draw</h3>
+                <h3 class="py-2 my-6 w-64 mb-24 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl md:mb-6">Draw</h3>
             </div>
             <div class="mx-auto">
                 <Link href="/kids/profile">
@@ -37,7 +37,7 @@
                     />
                     </div>
                 </Link>
-                <h3 class="py-2 my-6 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl">Profile</h3>
+                <h3 class="py-2 my-6 w-64 mb-24 bg-white bg-opacity-90 ring-3 ring-gray-300 border rounded-3xl text-center text-4xl md:mb-6">Profile</h3>
             </div>
       </div>
     </div>
