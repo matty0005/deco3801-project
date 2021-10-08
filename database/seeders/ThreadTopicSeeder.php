2082b2b38098ed443a8a5eb3a3bc409fde100b5f
BozenX <bozen0@hotmail.com>
2021-10-08 16:05:10 +1000

    changed thread topic seeder

--- database/seeders/ThreadTopicSeeder.php
+++ database/seeders/ThreadTopicSeeder.php
@@ -15,19 +15,27 @@ class ThreadTopicSeeder extends Seeder
     public function run()
     {
         DB::table('thread_topics')->insert([
-            'title' => 'Autism'
+            'title' => 'Autism',
+            'short_description' => 'Lorem ipsum dolor sit amet.',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacus erat, efficitur et arcu sed, vestibulum eleifend dui. Morbi congue lectus et nunc accumsan, nec bibendum dui blandit. Suspendisse potenti. Vestibulum arcu nisi, semper eget tortor et, semper fermentum mi. Nunc in nulla suscipit, rutrum massa a, facilisis mi. Aenean quis ultricies purus, eu rutrum arcu. Vestibulum porttitor interdum lorem, nec convallis dui volutpat quis.'
         ]);
 
         DB::table('thread_topics')->insert([
-            'title' => 'ADHD'
+            'title' => 'ADHD',
+            'short_description' => 'Lorem ipsum dolor sit amet.',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacus erat, efficitur et arcu sed, vestibulum eleifend dui. Morbi congue lectus et nunc accumsan, nec bibendum dui blandit. Suspendisse potenti. Vestibulum arcu nisi, semper eget tortor et, semper fermentum mi. Nunc in nulla suscipit, rutrum massa a, facilisis mi. Aenean quis ultricies purus, eu rutrum arcu. Vestibulum porttitor interdum lorem, nec convallis dui volutpat quis.'
         ]);
 
         DB::table('thread_topics')->insert([
-            'title' => 'OCD'
+            'title' => 'OCD',
+            'short_description' => 'Lorem ipsum dolor sit amet.',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacus erat, efficitur et arcu sed, vestibulum eleifend dui. Morbi congue lectus et nunc accumsan, nec bibendum dui blandit. Suspendisse potenti. Vestibulum arcu nisi, semper eget tortor et, semper fermentum mi. Nunc in nulla suscipit, rutrum massa a, facilisis mi. Aenean quis ultricies purus, eu rutrum arcu. Vestibulum porttitor interdum lorem, nec convallis dui volutpat quis.'
         ]);
 
         DB::table('thread_topics')->insert([
-            'title' => 'SLD'
+            'title' => 'SLD',
+            'short_description' => 'Lorem ipsum dolor sit amet.',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacus erat, efficitur et arcu sed, vestibulum eleifend dui. Morbi congue lectus et nunc accumsan, nec bibendum dui blandit. Suspendisse potenti. Vestibulum arcu nisi, semper eget tortor et, semper fermentum mi. Nunc in nulla suscipit, rutrum massa a, facilisis mi. Aenean quis ultricies purus, eu rutrum arcu. Vestibulum porttitor interdum lorem, nec convallis dui volutpat quis.'
         ]);
     }
 }
