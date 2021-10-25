1e79df22ddec924aea93a4b61d2aa85ec92cac71
Joshua W <joshuadwallace1@gmail.com>
2021-10-25 18:05:24 +1000

    npm run prod and fixed barely noticeable responsiveness issue

--- resources/js/Shared/Select.vue
+++ resources/js/Shared/Select.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div class="flex items-center text-small sm:text-base font-medium text-gray-700">
-            {{ label }}
+            {{ label }} 
         </div>
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 my-2 gap-x-4 sm:gap-x-6 md:gap-x-8">
+        <div class="grid grid-cols-2 md:grid-cols-4 my-2 gap-x-4 sm:gap-x-6 md:gap-x-8">
             <button v-for="option in options" :key="option" @click="selected(option)" type="button" 
             :class="optionsSelected.indexOf(option) >= 0 ? 'bg-parent-400 hover:bg-parent-500 text-parent-100' : 'bg-parent-100 hover:bg-parent-200 text-parent-800'"
             class="inline-flex items-center my-2 px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm focus:outline-none">
